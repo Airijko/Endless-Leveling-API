8f7a73d5313e69323b1961d9704caf4e1b16d5fb
jojoj <jojojimenajr@gmail.com>
2026-03-19 21:53:48 -0500

    dungeon balancing

---
+++ src/main/java/com/airijko/endlessleveling/shared/SharedEffectKind.java
@@ -0,0 +1,10 @@
+package com.airijko.endlessleveling.shared;
+
+/**
+ * Distinguishes where a shared effect comes from while keeping one common model.
+ */
+public enum SharedEffectKind {
+    AUGMENT,
+    ARCHETYPE_PASSIVE,
+    INNATE_PASSIVE
+}
