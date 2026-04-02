09707c8f106596b6632bd7757d060222dfb8c8c1
jojoj <jojojimenajr@gmail.com>
2026-04-01 23:10:25 -0500

    dungeons page completed

--- src/main/java/com/airijko/endlessleveling/ui/DungeonsUIPage.java
+++ src/main/java/com/airijko/endlessleveling/ui/DungeonsUIPage.java
@@ -5,6 +5,7 @@ import javax.annotation.Nonnull;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime;
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.entities.player.pages.InteractiveCustomUIPage;
 import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder;
 import com.hypixel.hytale.server.core.ui.builder.UIEventBuilder;
@@ -12,6 +13,9 @@ import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import static com.hypixel.hytale.protocol.packets.interface_.CustomUIEventBindingType.Activating;
+import static com.hypixel.hytale.server.core.ui.builder.EventData.of;
 
 public class DungeonsUIPage extends InteractiveCustomUIPage<SkillsUIPage.Data> {
 
@@ -23,6 +27,10 @@ public class DungeonsUIPage extends InteractiveCustomUIPage<SkillsUIPage.Data> {
     private static final String AZAROTH_IMAGE_PATH = IMAGE_SOURCE_BASE + "MajorAzarothPlaceholder.png";
     private static final String KATHERINA_IMAGE_PATH = IMAGE_SOURCE_BASE + "MajorKatherinaPlaceholder.png";
     private static final String BARON_IMAGE_PATH = IMAGE_SOURCE_BASE + "MajorBaronPlaceholder.png";
+    private static final String ENDGAME_MAIN_CLASS = "endgame.plugin.EndgameQoL";
+    private static final String MAJOR_MAIN_CLASS = "com.major76.majordungeons.MajorDungeons";
+    private static final String ENDGAME_DOWNLOAD_URL = "https://www.curseforge.com/hytale/mods/endgame-qol";
+    private static final String MAJOR_DOWNLOAD_URL = "https://www.curseforge.com/hytale/mods/major-dungeons";
 
     public DungeonsUIPage(@Nonnull PlayerRef playerRef,
             @Nonnull CustomPageLifetime lifetime) {
@@ -54,6 +62,18 @@ public class DungeonsUIPage extends InteractiveCustomUIPage<SkillsUIPage.Data> {
         setBannerImage(ui, "#AzarothBannerImage", AZAROTH_IMAGE_PATH);
         setBannerImage(ui, "#KatherinaBannerImage", KATHERINA_IMAGE_PATH);
         setBannerImage(ui, "#BaronBannerImage", BARON_IMAGE_PATH);
+
+        boolean endgameInstalled = isClassPresent(ENDGAME_MAIN_CLASS);
+        boolean majorInstalled = isClassPresent(MAJOR_MAIN_CLASS);
+        ui.set("#EndgameNotInstalledLabel.Visible", !endgameInstalled);
+        ui.set("#EndgameInstallButton.Visible", !endgameInstalled);
+        ui.set("#EndgameDimOverlay.Visible", !endgameInstalled);
+        ui.set("#MajorNotInstalledLabel.Visible", !majorInstalled);
+        ui.set("#MajorInstallButton.Visible", !majorInstalled);
+        ui.set("#MajorDimOverlay.Visible", !majorInstalled);
+
+        events.addEventBinding(Activating, "#EndgameInstallButton", of("Action", "dungeons:install:endgame"), false);
+        events.addEventBinding(Activating, "#MajorInstallButton", of("Action", "dungeons:install:major"), false);
 
         NavUIHelper.applyNavVersion(ui, playerRef, "dungeons",
             "Common/UI/Custom/Pages/Dungeons/DungeonsPage.ui",
@@ -106,6 +126,15 @@ public class DungeonsUIPage extends InteractiveCustomUIPage<SkillsUIPage.Data> {
         return false;
     }
 
+    private boolean isClassPresent(@Nonnull String className) {
+        try {
+            Class.forName(className);
+            return true;
+        } catch (Throwable ignored) {
+            return false;
+        }
+    }
+
     @Override
     public void handleDataEvent(@Nonnull Ref<EntityStore> ref,
             @Nonnull Store<EntityStore> store,
@@ -113,6 +142,18 @@ public class DungeonsUIPage extends InteractiveCustomUIPage<SkillsUIPage.Data> {
         super.handleDataEvent(ref, store, data);
 
         if (data.action == null || data.action.isEmpty()) {
+            return;
+        }
+
+        if ("dungeons:install:endgame".equalsIgnoreCase(data.action)) {
+            playerRef.sendMessage(Message.raw("[Endgame & QoL] Not installed on this server. Click the link below to download:").color("#d2ecff"));
+            playerRef.sendMessage(Message.raw(">> Click here to open the download page (CurseForge) <<").link(ENDGAME_DOWNLOAD_URL).color("#6fe3ff"));
+            return;
+        }
+
+        if ("dungeons:install:major".equalsIgnoreCase(data.action)) {
+            playerRef.sendMessage(Message.raw("[Major Dungeons] Not installed on this server. Click the link below to download:").color("#ffd58a"));
+            playerRef.sendMessage(Message.raw(">> Click here to open the download page (CurseForge) <<").link(MAJOR_DOWNLOAD_URL).color("#6fe3ff"));
             return;
         }
 
